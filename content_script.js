3c7a4493406e07eeeed5a40d7d9f9ef625de814f
Jonas <jonhe002@student.liu.se>
2014-11-27 19:35:26 +0100

    v1.22, more subs, more subtle

--- content_script.js
+++ content_script.js
@@ -1,8 +1,8 @@
 
 
-String.prototype.skwisgaarify = function(){
+function skwisgaarify(str){
     return(
-        this.replace( 
+        str.replace( 
             new RegExp("([cydrtpgklvbnm](?= ))", "gi"), 
             // Add the character s at the end of some random words (40% chance)
             function($1){ 
@@ -38,7 +38,7 @@ function walk(node)
 			break;
 
 		case 3: // Text node
-			if (Math.random()<0.1){
+			if (Math.random()<0.05){
 			handleText(node);
 			}
 			break;
@@ -52,10 +52,16 @@ function handleText(textNode)
 	v = v.replace(/\bis\b/g, "ams");
 	v = v.replace(/\bam\b/g, "ams");
 	v = v.replace(/\bfriends\b/g, "pals");
-	v = v.replace(/\that\b/g, "thats");
-	v = v.replace(/\cool\b/g, "brutal");
-	v = v.replace(/\TIL\b/g, "Todays I have learneds");
-	v = v.skwisgaarify();
+	v = v.replace(/\bthat\b/g, "thats");
+	v = v.replace(/\bcool\b/g, "brutal");
+	v = v.replace(/\bneat\b/g, "brutal");
+	v = v.replace(/\bgood\b/g, "brutal");
+	v = v.replace(/\bthanks\b/gi, "thanks (i guess)");
+	v = v.replace(/\bbrother\b/g, "brother --I MEANS BANDMATE--");
+	v = v.replace(/\bbrothers\b/g, "brothers --I MEANS BANDMATES--");
+	v = v.replace(/(:\()/g, "well maybe I should just KILL MYSELF. would THAT be brutal enough for you??");
+	v = v.replace(/\bTIL\b/g, "Todays I have learneds");
+	v = skwisgaarify(v);
 	v = v.replace(/\bfriend\b/g, "pal");
 	if (Math.random()<0.1){
 		v = v.replace(/\.$/g, ". Well, I guess THATS DOABLE.");
